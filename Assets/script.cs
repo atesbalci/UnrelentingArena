6d33b395b5ff48814fd3cbea3556bea8bea3a0d6
atesbalci <atesbalci@gmail.com>
2015-01-16 21:23:43 +0200

    click to move

---
+++ Assets/script.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class script : MonoBehaviour {
+	void Start () {
+		
+	}
+	
+	void Update () {
+//		ThirdPersonController c = gameObject.GetComponentInChildren<ThirdPersonController>();
+		if (Input.GetKey ("up")) {
+//			c._characterState = 3;
+		}
+		if (Input.GetKey ("down")) {
+
+		}
+	}
+}
