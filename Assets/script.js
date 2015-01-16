4577524a4082584fccc231f3f465441c2e8d1278
atesbalci <atesbalci@gmail.com>
2015-01-16 02:40:36 +0200

    tests

---
+++ Assets/script.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var acc:int = 10;
+
+function Start () {
+	
+}
+
+function Update () {
+	var amount:int = 0;
+	if (Input.GetKey ("up"))
+		amount += acc;
+	if (Input.GetKey ("down"))
+		amount -= acc;
+	
+	rigidbody.AddRelativeForce(amount, 0, 0);
+}
