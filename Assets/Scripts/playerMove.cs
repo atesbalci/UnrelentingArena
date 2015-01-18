2a34ddc9a034b6f371e0a3e2b4fc14f51263ef33
atesbalci <atesbalci@gmail.com>
2015-01-18 20:32:16 +0200

    camera fix and simplifications on player movement script

---
+++ Assets/Scripts/playerMove.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMove : MonoBehaviour {
+
+    private Transform myTransform;
+    private Vector3 destinationPosition;
+    private float destinationDistance;
+    private float clickMoveSpeed;
+
+    public float movementSpeed = 8;
+
+    void Start() {
+        myTransform = transform;
+        destinationPosition = myTransform.position;
+    }
+
+    void Update() {
+        destinationDistance = Vector3.Distance(destinationPosition, myTransform.position);
+
+        if (destinationDistance < .5f) {
+            clickMoveSpeed = 0;
+            animation.Play("idle");
+        } else if (destinationDistance > .5f) {
+            clickMoveSpeed = movementSpeed;
+            animation.Play("run");
+        }
+
+        if (Input.GetKey(KeyCode.Mouse1) && GUIUtility.hotControl == 0) {
+            Plane playerPlane = new Plane(Vector3.up, myTransform.position);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float hitdist = 0.0f;
+
+            if (playerPlane.Raycast(ray, out hitdist)) {
+                Vector3 targetPoint = ray.GetPoint(hitdist);
+                destinationPosition = ray.GetPoint(hitdist);
+                Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+                myTransform.rotation = targetRotation;
+            }
+        }
+
+        if (destinationDistance > .5f) {
+            myTransform.position = Vector3.MoveTowards(myTransform.position, destinationPosition, clickMoveSpeed * Time.deltaTime);
+        }
+    }
+}
