a8c18105552f885ab085af2f12d18fa001194886
heratio <eveon@live.com>
2015-01-23 21:22:05 +0200

    Main menu and buttons added with minimal functionality.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public void playButton() {
+        Debug.Log("Selams");
+        Application.LoadLevel("MainGame");
+    }
+
+
+    public void exitApp () {
+        Application.Quit();
+    }
+
+
+}
