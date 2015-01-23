1e8f166e2413d6e799f9746626b0b16363ce2759
atesbalci <atesbalci@gmail.com>
2015-01-23 19:15:53 +0200

    health syncronization

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -15,4 +15,15 @@ public class PlayerScript : MonoBehaviour {
     void Update() {
         player.update(gameObject);
     }
+
+    void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info) {
+        float health = 100;
+        if (stream.isWriting) {
+            health = player.health;
+            stream.Serialize(ref health);
+        } else {
+            stream.Serialize(ref health);
+            player.health = health;
+        }
+    }
 }
