ad7366026cd673d02ca8972df373622f1e68f2a0
atesbalci <atesbalci@gmail.com>
2015-02-03 00:45:43 +0200

    healthbars motherfucker, it draws and shit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,11 +9,27 @@ public class PlayerScript : MonoBehaviour {
     }
 
     void Start() {
-    
     }
 
     void Update() {
         player.update(gameObject);
+    }
+
+    void OnGUI() {
+        Vector2 targetPos;
+        targetPos = Camera.main.WorldToScreenPoint(transform.position);
+        targetPos.y += 70;
+        Texture health = Resources.Load("UI-Elements/healthabove") as Texture;
+
+        int width = 150;
+        int height = 40;
+
+        GUI.Box(new Rect(targetPos.x - width / 2, Screen.height - targetPos.y, width, height), "");
+        GUI.DrawTexture(new Rect(targetPos.x - width / 2 + 5, Screen.height - targetPos.y, (player.health / player.maxHealth) * (width - 10), height), health, ScaleMode.ScaleToFit, true, 10.0F);
+        GUIStyle style = GUIStyle.none;
+        style.richText = true;
+        style.alignment = TextAnchor.MiddleCenter;
+        GUI.TextField(new Rect(targetPos.x - width / 2, Screen.height - targetPos.y, width, height), "<color=#FFFFFF>" + (player.health + "/" + player.maxHealth) + "</color>", style);
     }
 
     void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info) {
