8132800776a739ca89a481e641d1c6a1e7b9a525
unknown <atesbalci@gmail.com>
2015-03-26 13:32:51 +0200

    minor changes

---
+++ Assets/Scripts/ParticleScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleScript : MonoBehaviour {
+    private ParticleSystem particleSystem;
+    private Quaternion rotation;
+
+	void Start () {
+        particleSystem = GetComponent<ParticleSystem>();
+        rotation = GetComponentInParent<Transform>().rotation;
+	}
+	
+	void Update () {
+        ParticleSystem.Particle[] particles = new ParticleSystem.Particle[100];
+        particleSystem.GetParticles(particles);
+        for (int i = 0; i < particles.Length; i++) {
+            particles[i].axisOfRotation -= Vector3.up;
+            particles[i].rotation = rotation.y;
+        }
+        particleSystem.SetParticles(particles, 100);
+	}
+}
--- Assets/Scripts/UI-Scripts/HealthDisplayScript.cs
+++ Assets/Scripts/UI-Scripts/HealthDisplayScript.cs
@@ -3,19 +3,31 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class HealthDisplayScript : MonoBehaviour {
+    public Image lostHealth;
+
+    private Image health;
+
     private Player player;
 
     private Color fullHealth;
     private Color lowHealth;
+    private float previous;
 
     void Start() {
         player = GetComponentInParent<PlayerScript>().player;
+        health = GetComponent<Image>();
         fullHealth = Color.green;
         lowHealth = Color.red;
+        previous = 1.0f;
     }
 
     void Update() {
-        GetComponent<Image>().fillAmount = player.health / player.maxHealth;
-        GetComponent<Image>().color = Color.Lerp(lowHealth, fullHealth, player.health / player.maxHealth);
+        float ratio = player.health / player.maxHealth;
+        health.fillAmount = ratio;
+        health.color = Color.Lerp(lowHealth, fullHealth, ratio);
+        if (ratio - previous > 0.001 || ratio - previous < 0.001) {
+            previous = Mathf.Lerp(previous, ratio, 0.1f);
+        }
+        lostHealth.fillAmount = previous;
     }
 }
